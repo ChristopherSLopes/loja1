9bd71592aa4f3f5dce19d8656ae93382cc3e99f7
ChristopherSLopes <christophercsl95@gmail.com>
2023-07-04 16:15:44 -0300

    oi

--- pages/contato.js
+++ pages/contato.js
@@ -6,9 +6,9 @@ const Contato = () => {
         <div>
             <PageTitle title='Contato'/>
             <h1 className='titulo'>Contato</h1>
+            <p>Endereço: Avenida Brasil, 10000, Centro de Foz do Iguaçu/PR - CEP 85851-000</p>
             <h2>Qualquer duvida, entre em contato</h2>
             <p>Instagram</p>
-            <p>Facebook</p>
             <p>WhatsApp: (45) 0000-0000</p>
         </div>
     )
